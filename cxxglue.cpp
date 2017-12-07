cf0f998381bc1c40b2c1e6bf4bd13ea276f04b4f
Srijan Kumar Sharma <rajasrijan@users.noreply.github.com>
2017-12-07 18:31:37 +0530

    fix c++ break

--- cxxglue.cpp
+++ cxxglue.cpp
@@ -1,37 +1,61 @@
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
-
+extern int get_spin_lock(void *);
+extern int release_spin_lock(void *);
 void printf(const char *format, ...);
-#define assert(x) if(x!=0){printf("Assert at [%s]",#x);__asm__("cli;hlt;");}
-
+int __cxa_guard_acquire(unsigned long int *guard_object);
+void __cxa_guard_release(unsigned long int *guard_object);
+#define assert(x)                     \
+	if (x != 0)                       \
+	{                                 \
+		printf("Assert at [%s]", #x); \
+		__asm__("cli;hlt;");          \
+	}
+unsigned long int guard = 0;
+int __cxa_guard_acquire(unsigned long int *guard_object)
+{
+	if (*guard_object)
+		return 0;
+	get_spin_lock(&guard);
+	if (*guard_object)
+	{
+		release_spin_lock(&guard);
+		return 0;
+	}
+	return 1;
+}
+void __cxa_guard_release(unsigned long int *guard_object)
+{
+	*guard_object = 1;
+	release_spin_lock(&guard);
+}
 void *__dso_handle;
 void *__gxx_personality_v0;
 void __cxa_pure_virtual(void);
 
 struct
 {
-    void (*destructor) (void *);
-    void *arg;
-    void *dso;
+	void (*destructor)(void *);
+	void *arg;
+	void *dso;
 } __atexit_funcs[100];
 unsigned int fn_count = 0;
 
-int __cxa_atexit(void (*destructor) (void *), void *arg, void *dso);
+int __cxa_atexit(void (*destructor)(void *), void *arg, void *dso);
 
-int __cxa_atexit(void (*destructor) (void *), void *arg, void *dso)
+int __cxa_atexit(void (*destructor)(void *), void *arg, void *dso)
 {
-    __atexit_funcs[fn_count].destructor = destructor;
-    __atexit_funcs[fn_count].arg = arg;
-    __atexit_funcs[fn_count].dso = dso;
-    fn_count++;
+	__atexit_funcs[fn_count].destructor = destructor;
+	__atexit_funcs[fn_count].arg = arg;
+	__atexit_funcs[fn_count].dso = dso;
+	fn_count++;
 }
 
 void __cxa_pure_virtual()
 {
-    // Do nothing or print an error message.
-    assert(1);
+	// Do nothing or print an error message.
+	assert(1);
 }
 namespace __cxxabiv1
 {
